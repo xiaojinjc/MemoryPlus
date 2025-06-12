4bf22ca2ca1232990080d03fc4f57a4f3c6552bc
Xiaojin <xxj109@student.bham.ac.uk>
2025-06-13 00:15:36 +0200

    added main colour, sdk change, created main activity xml, modified entry

--- app/src/main/java/com/example/memoryplus/model/Entry.java
+++ app/src/main/java/com/example/memoryplus/model/Entry.java
@@ -5,12 +5,14 @@ public class Entry {
     private String category;
     private String type;
     private String description;
+    private String notes;
 
-    public Entry(String date, String category, String type, String description){
+    public Entry(String date, String category, String type, String description, String notes){
         this.date = date;
         this.category = category;
         this.type = type;
         this.description = description;
+        this.notes = notes;
     }
 
     // Getters
@@ -18,11 +20,13 @@ public class Entry {
     public String getCategory() { return category; }
     public String getType() { return type; }
     public String getDescription() { return description; }
+    public String getNotes() { return notes; }
 
     // Setters
     public void setDate(String date) { this.date = date; }
     public void setCategory(String category) { this.category = category; }
     public void setType(String type) { this.type = type; }
     public void setDescription(String description) { this.description = description; }
+    public void setNotes(String notes) { this.description = notes; }
 
 }
