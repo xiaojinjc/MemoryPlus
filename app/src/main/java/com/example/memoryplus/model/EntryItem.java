f47305b5cd5fb5ca7f85ee319fe0dab5c2d1e7a7
Xiaojin <xxj109@student.bham.ac.uk>
2025-06-23 02:09:25 +0200

    added notes section to EntryItem item

--- app/src/main/java/com/example/memoryplus/model/EntryItem.java
+++ app/src/main/java/com/example/memoryplus/model/EntryItem.java
@@ -4,11 +4,13 @@ public class EntryItem implements ListItem {
     public String type;
     public String description;
     public String part;
+    public String notes;
 
-    public EntryItem(String type, String description, String part) {
+    public EntryItem(String type, String description, String part, String notes) {
         this.type = type;
         this.description = description;
         this.part = part;
+        this.notes = notes;
     }
 
     @Override
