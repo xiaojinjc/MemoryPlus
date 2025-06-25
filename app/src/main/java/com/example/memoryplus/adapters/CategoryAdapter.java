5bb10b0358cac4b29ad0aab34628e95807122a33
Xiaojin <xxj109@student.bham.ac.uk>
2025-06-25 23:18:48 +0200

    Added category adapter to show categories in list

--- app/src/main/java/com/example/memoryplus/adapters/CategoryAdapter.java
+++ app/src/main/java/com/example/memoryplus/adapters/CategoryAdapter.java
@@ -1,2 +1,50 @@
-package com.example.memoryplus.adapters;public class CategoryAdapter {
+package com.example.memoryplus.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.memoryplus.R;
+import com.example.memoryplus.entity.Category;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.MyViewHolder> {
+    private List<Category> itemList = new ArrayList<>();
+
+    public void setItemList(List<Category> newCategories){
+        itemList = newCategories;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.category_item, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Category item = itemList.get(position);
+        holder.text.setText(item.name);
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView text;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            text = itemView.findViewById(R.id.categoryText);
+        }
+    }
 }
