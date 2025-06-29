715c398d9a0eefad0580f4781fb90f859d722772
Xiaojin <x.jinjiangchen@gmail.com>
2025-06-29 23:42:36 +0200

    created initial activity/layout for type creation

---
+++ app/src/main/java/com/example/memoryplus/TypeActivity.java
@@ -0,0 +1,14 @@
+package com.example.memoryplus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.type_activity);
+    }
+}
