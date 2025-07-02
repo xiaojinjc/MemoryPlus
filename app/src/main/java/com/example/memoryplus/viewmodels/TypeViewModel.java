6f2ff977e7291b3a5b024ad73a7b2675d7da11e1
Xiaojin <x.jinjiangchen@gmail.com>
2025-07-02 22:55:38 +0200

    Added TypeViewModel.java

--- app/src/main/java/com/example/memoryplus/viewmodels/TypeViewModel.java
+++ app/src/main/java/com/example/memoryplus/viewmodels/TypeViewModel.java
@@ -1,2 +1,32 @@
-package com.example.memoryplus.viewmodels;public class TypeViewModel {
+package com.example.memoryplus.viewmodels;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.memoryplus.entities.Category;
+import com.example.memoryplus.entities.Type;
+import com.example.memoryplus.repositories.TypeRepository;
+
+import java.util.List;
+
+public class TypeViewModel extends AndroidViewModel {
+    private final TypeRepository repository;
+    LiveData<List<Type>> allTypes;
+
+    public TypeViewModel(@NonNull Application application) {
+        super(application);
+        repository = new TypeRepository(application);
+        allTypes = (LiveData<List<Type>>) repository.getAll();
+    }
+
+    public LiveData<List<Type>> getAllTypes() { return allTypes; }
+
+    public void insertType(Type type) { repository.insert(type); }
+
+    public void deleteType(Type type) { repository.delete(type); }
+
+//    TODO: add method for getTypeListByCat, Livedata?
 }
