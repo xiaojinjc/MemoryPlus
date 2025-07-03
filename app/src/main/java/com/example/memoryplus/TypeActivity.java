74e067f877f1e4504e57752cafd07bebd6258bae
Xiaojin <x.jinjiangchen@gmail.com>
2025-07-03 05:17:47 +0200

    Added create type functionality, override toString

--- app/src/main/java/com/example/memoryplus/TypeActivity.java
+++ app/src/main/java/com/example/memoryplus/TypeActivity.java
@@ -7,11 +7,16 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.memoryplus.adapters.TypeAdapter;
 import com.example.memoryplus.entities.Category;
+import com.example.memoryplus.entities.Type;
 import com.example.memoryplus.viewmodels.CategoryViewModel;
 import com.example.memoryplus.viewmodels.TypeViewModel;
 
@@ -45,17 +50,55 @@ public class TypeActivity extends AppCompatActivity {
             adapter.setItemList(types);
         });
 
+        Button addType = findViewById(R.id.addType);
+        EditText typeInput = findViewById(R.id.typeInput);
+        List<Type> currentList = typeViewModel.getAllTypes().getValue();
+
+        addType.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String typeName = typeInput.getText().toString().trim();
+
+//                Validation
+                if (spinner.getSelectedItem() == null){
+                    Toast.makeText(TypeActivity.this, "No category has been selected", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (typeName.isEmpty()){
+                    Toast.makeText(TypeActivity.this, "No category has been selected", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (currentList != null){
+                    boolean duplicate = false;
+                    for (Type t : currentList) {
+                        if (t.name.equalsIgnoreCase(typeName)){
+                            duplicate = true;
+                            break;
+                        }
+                    }
+
+                    if (duplicate) {
+                        Toast.makeText(TypeActivity.this, "No category has been selected", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                }
+
+                Category cat = (Category) spinner.getSelectedItem();
+                Type newType = new Type(cat.id, typeName);
+                typeViewModel.insertType(newType);
+            }
+        });
     }
 
     private void updateSpinner(List<Category> categories) {
 
-        List<String> categoryNames = new ArrayList<>();
-        for (Category c : categories) {
-            categoryNames.add(c.name);
-            Log.d("SpinnerDebug", "category" + categoryNames.toString());
-        }
+//        List<String> categoryNames = new ArrayList<>();
+//        for (Category c : categories) {
+//            categoryNames.add(c.name);
+//            Log.d("SpinnerDebug", "category" + categoryNames.toString());
+//        }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, categoryNames);
+        ArrayAdapter<Category> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, categories);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
     }
--- app/src/main/java/com/example/memoryplus/entities/Category.java
+++ app/src/main/java/com/example/memoryplus/entities/Category.java
@@ -16,4 +16,9 @@ public class Category {
     public Category(@NonNull String name){
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
